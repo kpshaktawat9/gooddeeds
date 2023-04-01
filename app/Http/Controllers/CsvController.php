93d4cbc1fbf7420629cce7ad746939f0ae561555
kpshaktawat9 <k.p.shaktawat9@gmail.com>
2023-04-01 23:54:43 +0530

    csv data inserted

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -5,30 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Exports\CsvExport;
 use Excel;
+use App\Models\student;
+use DB;
 
 class CsvController extends Controller
 {
     //
     public function export()
     {
-        $users = [
-            [
-                'id' => 1,
-                'name' => 'Hardik',
-                'email' => 'hardik@gmail.com'
-            ],
-            [
-                'id' => 2,
-                'name' => 'Vimal',
-                'email' => 'vimal@gmail.com'
-            ],
-            [
-                'id' => 3,
-                'name' => 'Harshad',
-                'email' => 'harshad@gmail.com'
-            ]
-        ];
+        $users = [];
 
-        return Excel::download(new CsvExport($users),'user.csv');
+        if (($open = fopen(storage_path() . "/app/export/user.csv", "r")) !== FALSE) {
+                
+            while (($data = fgetcsv($open, 1000, ",")) !== FALSE) {
+                $users[] = $data;
+            }
+
+            fclose($open);
+        }
+
+        array_shift($users);
+        // print_r($users);
+
+        foreach($users as $row)
+        {
+            DB::table('students')->insert([
+                'id' => $row[0],
+                'name' => $row[1],
+                'email' => $row[2],
+                'class' => $row[3],
+                'school' => $row[4],
+                'total_score' => $row[5],
+                'address' => $row[6],
+                'phone' => $row[7],
+            ]);
+        }
     }
 }
